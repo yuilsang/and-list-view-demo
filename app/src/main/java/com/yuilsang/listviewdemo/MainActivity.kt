d4ee8301c99ba9decfb59a61587f132e6295c753
ilsang.yu <yuilsang@gmail.com>
2019-10-21 00:45:45 +0900

    demo 0부터 100 까지 리스트뷰에 노출

--- app/src/main/java/com/yuilsang/listviewdemo/MainActivity.kt
+++ app/src/main/java/com/yuilsang/listviewdemo/MainActivity.kt
@@ -2,11 +2,26 @@ package com.yuilsang.listviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 데이터 구성
+        var demo1DataList = arrayListOf("Demo 0", "Demo 1", "Demo 2", "Demo 3")
+
+        for (i in 4..100) {
+            demo1DataList.add("Demo $i")
+        }
+
+        // 어댑터 설정
+        val demo1DataListAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, demo1DataList)
+
+        // 리스트 뷰 설정
+        val listView = findViewById<ListView>(R.id.list_view)
+        listView.adapter = demo1DataListAdapter
     }
 }
